b7a9b14d4828230de0e363d69161fd862503a12d
Pavel Kozlov <Pavel Kozlov>
2023-06-29 21:21:12 +0300

    some random tasks

--- task.py
+++ task.py
@@ -1,41 +1,33 @@
-# def insert_dash(num):
-#
-#   digits = [d for d in str(num)]
-#
-#   for i in range(len(digits)-1):
-#     if int(digits[i])%2 and int(digits[i+1])%2:
-#         digits[i] = digits[i] + '-'
-#
-#   return ''.join(digits)
-#
-#
-# print(insert_dash(454793))
+def insert_dash(num):
 
-# def find_even_index(arr):
-#     for i in range(len(arr)):
-#         if sum(arr[:i]) == sum(arr[i+1:]):
-#             return i
- #     return -1
-#
-# print(find_even_index([1,2,3,4,3,2,1]))
+  digits = [d for d in str(num)]
+
+  for i in range(len(digits)-1):
+    if int(digits[i])%2 and int(digits[i+1])%2:
+        digits[i] = digits[i] + '-'
+
+  return ''.join(digits)
 
 
-# def high(x):
-#     return max(x.split(), key=lambda k: sum(ord(c) - 96 for c in k))
-#
-#
-# print(high('man i need a taxi up to ubud'))
+print(insert_dash(454793))
 
-# def two_sum(numbers, target):
-#     required = {}
-#     for i in range(len(numbers)):
-#         if target - numbers[i] in required:
-#             return [required[target - numbers[i]],i]
-#         else:
-#             required[numbers[i]]=i
-#
-#
-# print(two_sum([1234,5678,9012], 14690))
+
+def high(x):
+    return max(x.split(), key=lambda k: sum(ord(c) - 96 for c in k))
+
+
+print(high('man i need a taxi up to ubud'))
+
+def two_sum(numbers, target):
+    required = {}
+    for i in range(len(numbers)):
+        if target - numbers[i] in required:
+            return [required[target - numbers[i]],i]
+        else:
+            required[numbers[i]]=i
+
+
+print(two_sum([1234,5678,9012], 14690))
 
 
 # def encode_rot13(s):
